d3c0cdfc0c6c1890b5efb307eb04aaf275baef57
Cody-Joe Abe <cody.masao@gmail.com>
2019-02-08 17:47:47 -0800

    updated some css

--- app/components/SSCProject.jsx
+++ app/components/SSCProject.jsx
@@ -6,34 +6,51 @@ import SevenSegmentCounter from 'seven-segment-counter';
 const buttonTags = {increment: "+", decrement:"-"};
 
 
-const SSCProject = styled.div
-`
-  background-color: #EEDDCC;
-
+const SSCProjectDiv= styled.div
 `
 
-const SSCProjectHeader = styled.div
 `
 
-  background-color: #CCDDEE;
-
+const SSCProjectHeader = styled.h1
+`
+  text-align: left;
+  font-size: 40px;
+  padding-left: 20px;
 `
 
 const SSCProjectBody = styled.div
 `
+  text-align: left;
+  padding-left: 20px;
+  padding-bottom: 20px;
+`
+const SSCDiv = styled.div
+`
 
 `
 
-export default () => (
+class SSCProject extends React.Component{
+  constructor(props){
+    super(props);
+  }
 
-  <SSCProject>
-    <SSCProjectHeader>
-      ProjectHeader
-    </SSCProjectHeader>
-    <SSCProjectBody>
-      Project Body
-      <SevenSegmentCounter buttonClass="buttonClass" buttonTag={buttonTags} width={600} onColor={"#000000"} offColor={"#BBEEBB"} backgroundColor={"#AAFFAA"}/>
-    </SSCProjectBody>
-  </SSCProject>
 
-)
+  render(){
+    return(
+    <SSCProjectDiv>
+      <SSCProjectHeader>
+        Seven Segment Counter Node Module
+      </SSCProjectHeader>
+      <SSCProjectBody>
+        This project was inspired by the Seven Segment Display on this <a target="_blank" href="https://youtu.be/MlRlgbrAVOs">YouTube video</a> by the "The Coding Train" and I extended it to be a counter. I also added the feature of being able to set it to a certain value programmatically as well as the ablilty to change its color scheme.
+
+      </SSCProjectBody>
+      <SSCDiv>
+        <SevenSegmentCounter buttonClass="buttonClass" buttonTag={buttonTags} width={600} onColor={"#000000"} offColor={"#BBEEBB"} backgroundColor={"#AAFFAA"}/>
+      </SSCDiv>
+    </SSCProjectDiv>
+    )
+  }
+}
+
+export default SSCProject;
--- app/pages/Projects.jsx
+++ app/pages/Projects.jsx
@@ -4,13 +4,15 @@ import Page from '../components/Page.jsx';
 import Contents from '../components/Contents.jsx';
 import SSCProject from '../components/SSCProject.jsx';
 
+import projectDesc from '../resources/descriptions.json';
+
 const Projects = () => (
   <Page>
     <Helmet>
       <title>About Page</title>
     </Helmet>
     <Contents>
-      <SSCProject />
+      <SSCProject description={projectDesc.ssc}/>
     </Contents>
 
   </Page>
