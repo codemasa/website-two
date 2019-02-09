18da828a9f9ec493a63b5beb6b6476f59846e34b
Cody-Joe Abe <cody.masao@gmail.com>
2019-02-08 17:53:56 -0800

    finished a section

--- app/components/SSCProject.jsx
+++ app/components/SSCProject.jsx
@@ -42,7 +42,9 @@ class SSCProject extends React.Component{
         Seven Segment Counter Node Module
       </SSCProjectHeader>
       <SSCProjectBody>
-        This project was inspired by the Seven Segment Display on this <a target="_blank" href="https://youtu.be/MlRlgbrAVOs">YouTube video</a> by the "The Coding Train" and I extended it to be a counter. I also added the feature of being able to set it to a certain value programmatically as well as the ablilty to change its color scheme.
+        <p>This project was inspired by the Seven Segment Display on this <a target="_blank" href="https://youtu.be/MlRlgbrAVOs">YouTube video</a> by the "The Coding Train" and I extended it to be a counter. I also added the feature of being able to set it to a certain value programmatically as well as the ablilty to change its color scheme.</p>
+        <br/>
+        <p>Find out more on <a target="_blank" href="https://github.com/codemasa/sevensegmentcounter">GitHub</a> or <a target="_blank" href="https://www.npmjs.com/package/seven-segment-counter">NPM</a></p>
 
       </SSCProjectBody>
       <SSCDiv>
